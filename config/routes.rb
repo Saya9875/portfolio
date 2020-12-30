897f915183d054d821906d752117bcf798c919e9
KATE <kate@KATEnoMacBook-Pro.local>
2020-12-30 10:40:24 +0900

    users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  'static_pages/show'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :users, only: [:show]
 end
