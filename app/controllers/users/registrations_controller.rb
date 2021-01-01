9c0bd5a9beb88f902353c16bc67bd7c1c367ab9f
KATE <amsh222vegan@gmail.com>
2021-01-01 18:40:18 +0900

    マイページ編集後の遷移先変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,7 +22,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update
     if @user.update(user_params)
-      redirect_to user_path(current_user)
+      redirect_to root_path
     else
       render :edit
     end
