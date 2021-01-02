3e619e6f0374265d9e24106b3aa3afdf118b4c04
KATE <amsh222vegan@gmail.com>
2021-01-02 20:33:35 +0900

    フォローの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     #@user.avatar.attach(params[:avatar])
     @microposts = @user.microposts.where(params[:id])
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.all
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.where(params[:id])
+    render 'show_follow'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :users, only: [:show] do
+  resources :users, only: [:index, :show] do
     member do
       get :following, :followers
     end
