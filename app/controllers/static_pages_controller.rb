4a2a7d437857f924cc2c5b761aa74cdc30fe0b9b
KATE <amsh222vegan@gmail.com>
2021-01-02 22:19:26 +0900

    nameをパラメータに追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
+      @user = current_user
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.where(params[:id])
-      #@user.avatar.attach(params[:user][:avatar])
+      @user.avatar.attach(params[:user][:avatar])
     end
   end
 end
