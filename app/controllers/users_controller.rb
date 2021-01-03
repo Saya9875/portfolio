be6d6c22f0fa432d235c8090eda167ac0856f04c
KATE <amsh222vegan@gmail.com>
2021-01-03 21:45:14 +0900

    ユーザーの削除機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.all
@@ -8,6 +9,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.where(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def following
@@ -29,5 +36,10 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :profile, :email, :password,
                                   :password_confirmation, :avatar)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
