a3176cd3d532fdba3fe460ef24ebaa9bf613af9d
KATE <amsh222vegan@gmail.com>
2021-01-05 18:55:15 +0900

    いいね機能エラー解消

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :iine_users, through: :likes, source: :user
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
